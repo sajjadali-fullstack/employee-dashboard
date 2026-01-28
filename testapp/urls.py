5a4239c8efcc89dc774b7ffe87693bb97def633d
your sajju <sajjadali.dev01@gmail.com>
2026-01-28 23:27:42 +0530

    Add another  more functionality like Edit btn Delete Btn in employee_detail.htmlm etc,

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<int:id>/',views.employee_detail_view, name='employee_details'),
-    path('add/', views.add_employee_view, name='add_employee')
+    path('add/', views.add_employee_view, name='add_employee'),
+    path('edit/<int:pk>/', views.edit_employee_view, name='edit_employee'),
+    path('delete/<int:pk>/', views.delete_employee, name='delete_employee'),
+
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from testapp.models import Employee
 from testapp.forms import EmployeeForm
 
@@ -34,3 +34,33 @@ def add_employee_view(request):  # Form view
         form = EmployeeForm()
 
     return render(request, 'testapp/add_employee.html', {'form':form})
+
+
+
+from django.contrib import messages
+
+# Edit Employee
+
+def edit_employee_view(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method =='POST':
+        form = EmployeeForm(request.POST, request.FILES, instance=employee)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Employee updated successfully')
+            return redirect('/')  # Redirect to the home page after sucessfull POST
+    else:
+        form = EmployeeForm(instance=employee)
+    context = {'form': form, 'employee': employee}
+    return render(request, 'testapp/add_employee.html', context)
+
+
+# Delete Employee
+def delete_employee(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        messages.success(request, 'Employee deleted successfully')
+        return redirect('/')  # Redirect to the home page
+    else:
+        return redirect(request, 'delete_employee.html', {'employee': employee})
